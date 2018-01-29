e434af121131046837a7ba17a966b356abadd374
Shibu Salam <pshibusalam@rediffmail.com>
2018-01-29 22:25:52 +0530

    corrected the errors

--- simple_server/app.js
+++ simple_server/app.js
@@ -8,20 +8,24 @@ const fs   = require('fs');
 const host = '127.0.0.1';
 const port = '3001';
 
-fs.readFile('index.html', (err, html) => {
+fs.readFile('./index.html', (err, html) => {
     if (err) {
-        console.error('error reading the \'index.html\' file ')
+        console.error('error reading the \'index.html\' file ');
     } else {
 
-        const server = http.createServer(host, (req, res)=> {
-            console.log('received request')
+        const server = http.createServer((req, res)=> {
+            console.log('received request');
             res.statusCode = 200;
-            res.setHeader = {'Content-Type':'text/html'};
+            res.setHeader('Content-Type', 'text/html');
+            res.write(html);
+            res.end();
         
         });
         
-        server.listen(port);
-        console.log('Server started!! Listening on '+host+':'+port);
+        server.listen(port, host, ()=>{
+            console.log('Server started!! Listening on '+host+':'+port);
+        });
+        
     }
 })
 
